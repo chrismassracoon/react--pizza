318ecc340ce72f2067750311881c710fc5d0fedf
Arthur <artyruakobhuk@gmail.com>
2022-10-07 21:02:32 +0300

    create react app and components with html && scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './scss/app.scss'
+import Categories from './components/categories/Categories';
+import Header from './components/header/Header';
+import Pizza from './components/pizza/Pizza';
+import Sort from './scss/components/sort/Sort';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	return (
+<div className="wrapper">
+     <Header/>
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+			 <Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+		<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza> 
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+	<Pizza title={'Чизбургер-пицца'} price={392} img={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}>
+	</Pizza>
+</div>
+        </div>
+      </div>
     </div>
-  );
+	);
 }
 
 export default App;
